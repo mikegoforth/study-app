6134d6ca410792f2836e0fc7b368326430d2fa17
Mike Goforth <mike.goforth@outlook.com>
2023-04-20 02:22:38 -0500

    fix: temp disable getStaticProps to address deploy failure

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,16 +1,15 @@
-import Meta, { defaultMetaProps } from '@/components/layout/meta';
-export { getStaticProps } from '.';
+// import Meta, { defaultMetaProps } from '@/components/layout/meta';
+// export { getStaticProps } from '.';
 
 export default function Custom404() {
   return (
     <div className="h-screen w-full flex justify-center items-center bg-black">
-      <Meta
+      {/* <Meta
         props={{
           ...defaultMetaProps,
-          title: '404 | MongoDB Starter Kit',
-          ogUrl: 'https://mongodb.vercel.app/404'
+          title: '404',
         }}
-      />
+      /> */}
       <h1 className="text-2xl font-light text-white">
         404 <span className="mx-3 text-4xl">|</span> User Not Found
       </h1>
